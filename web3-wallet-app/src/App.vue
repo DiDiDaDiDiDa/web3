bc5cb181043786b9ed52b89e8e4d452df03527a2
wangzhenyu <1904505316@qq.com>
2023-03-28 18:23:41 +0800

    钱包账户组件开发完成

--- web3-wallet-app/src/App.vue
+++ web3-wallet-app/src/App.vue
@@ -1,24 +1,23 @@
-
 <script setup>
-import {ref,onMounted } from 'vue'
-import { Button } from 'vant'
-import ButtonVue from './components/Button.vue'
-import AccountList from './components/AccountList.vue'
-import store2 from "store2"
+import { ref, onMounted } from "vue";
+import ButtonVue from "./components/Button.vue";
+import AccountList from "./components/AccountList.vue";
+import store2 from "store2";
 
-const walletInfo=ref(null)
+const walletInfo = ref([]);
 
-onMounted(()=>{
-  walletInfo.value=store2.get("walletInfo")||[]
-  console.log(walletInfo.value,"=====")
-})
-
+onMounted(() => {
+  walletInfo.value = store2.get("walletInfo") || [];
+});
 </script>
+
 <template>
   <ButtonVue></ButtonVue>
-  <AccountList :wallet-info='walletInfo'></AccountList>
-
+  <AccountList :wallet-info="walletInfo"></AccountList>
 </template>
 
 <style lang="less">
+body {
+  padding: 10px;
+}
 </style>
--- web3-wallet-app/src/components/Button.vue
+++ web3-wallet-app/src/components/Button.vue
@@ -1,119 +1,97 @@
-
 <script setup>
-import {ref} from 'vue'
-import {showNotify} from 'vant'
-import "vant/es/dialog/style"
-import "vant/es/notify/style"
-import * as bip39 from 'bip39'
+import { ref } from "vue";
+import { showNotify } from "vant";
+import * as bip39 from "bip39";
+import { hdkey } from "ethereumjs-wallet";
+import store2 from "store2";
 
-import ethWallet ,{ hdkey }from "ethereumjs-wallet"
-import store2 from "store2"
+import "vant/es/dialog/style";
+import "vant/es/notify/style";
 
-const show = ref(false)
-const password = ref('')
-const mnomonic= ref('')
-const mnomonicNew= ref('')
-const showMn=ref(false)
-const showMnDialog=ref(false)
+const show = ref(false);
+const password = ref("");
+const mnemonic = ref("");
+const mnemonic2 = ref("");
+const showMn = ref(false);
+const showMnDialog = ref(false);
 
+const createWallet = () => {
+    show.value = true;
+};
 
+const confirmPassword = () => {
+    if (password.value === "") {
+        showNotify({ type: "danger", message: "请输入密码" });
+    } else {
+        const walletInfo = store2.get("walletInfo");
+        mnemonic.value = walletInfo
+            ? walletInfo[0]["mnemonic"]
+            : bip39.generateMnemonic();
 
-const createWallet=() =>{
-    show.value = true
-}
-const confirmPassword =()=>{
-    if (password.value.length==0){
-        showNotify({ type: 'danger', message: '请输入密码' });
-    }else{
-        const walletInfo = store2.get("walletInfo")
-        mnomonic.value=walletInfo
-        ?walletInfo[0]['mnomonic']
-        :bip39.generateMnemonic()
-        if(walletInfo){
-            confirmMn()
-        }else{
-            showMn.value=true
+        if (walletInfo) {
+            confirmMn();
+        } else {
+            showMn.value = true;
         }
     }
-}
-const confirmSaveNmonic=()=>{
-    showMnDialog.value=true
-}
-const confirmMn=async()=>{
-        showMn.value=false
-        const storeWallet = store2.get("walletInfo")||[]
-        if(mnomonic.value!==mnomonicNew.value&&storeWallet.length==0){
-            return
-        }else{
-            const seed=await bip39.mnemonicToSeed(mnomonic.value)
-            const hdWallet=hdkey.fromMasterSeed(seed)
-            const addressIndex=storeWallet.length
-            const keyPair=hdWallet.derivePath(`m/44'/60'/0'/0/${addressIndex}`)
-            const wallet=keyPair.getWallet()
+};
 
-            const lowerCaseAddress=wallet.getAddressString()
-            const CheckSumAddress=wallet.getChecksumAddressString()
-            const privKey=wallet.getPrivateKey().toString("hex")
-            const keyStore=await wallet.toV3(password.value)
-            const walletObject={
-                id:addressIndex,
-                address:lowerCaseAddress,
-                privateKey:privKey,
-                keyStore,
-                mnomonic:mnomonic.value,
-                balance:0,
-            }
-            storeWallet.push(walletObject)
-            store2("walletInfo",storeWallet)
-        }
-}
+const confirmSaveMnemonic = () => {
+    showMnDialog.value = true;
+};
+
+const confirmMn = async () => {
+    showMn.value = false;
+    const storeWallet = store2.get("walletInfo") || [];
+    if (mnemonic.value !== mnemonic2.value && storeWallet.length === 0) {
+        return;
+    }
+    const seed = await bip39.mnemonicToSeed(mnemonic.value);
+    const hdWallet = hdkey.fromMasterSeed(seed);
+    const addressIndex = storeWallet.length === 0 ? 0 : storeWallet.length + 1;
+    const keyPair = hdWallet.derivePath(`m/44'/60'/0'/0/${addressIndex}`);
+    const wallet = keyPair.getWallet();
+
+    const lowerCaseAddress = wallet.getAddressString();
+    const CheckSumAddress = wallet.getChecksumAddressString();
+    const privateKey = wallet.getPrivateKey().toString("hex");
+    const keyStore = await wallet.toV3(password.value);
+    const walletObj = {
+        id: addressIndex,
+        address: lowerCaseAddress,
+        privateKey,
+        keyStore,
+        mnemonic: mnemonic.value,
+        balance: 0,
+    };
+    storeWallet.push(walletObj);
+    store2("walletInfo", storeWallet);
+};
 </script>
+
 <template>
     <van-space>
         <van-button type="primary" @click="createWallet">创建钱包</van-button>
         <van-button type="primary">导入钱包</van-button>
+        <van-dialog v-model:show="show" title="请输入密码" show-cancel-button @confirm="confirmPassword">
+            <van-cell-group inset>
+                <van-field v-model="password" label="密码：" placeholder="请输入密码" type="password" />
+            </van-cell-group>
+        </van-dialog>
     </van-space>
-    
-    <van-dialog v-model:show="show" 
-                title="请输入密码" 
-                show-cancel-button
-                @confirm="confirmPassword">
-        <van-cell-group inset>
-            <van-field v-model="password"
-                label="密码"
-                placeholder="请输入密码" 
-                type="password"
-                />
-        </van-cell-group>
-    </van-dialog>
-    
-    <template  v-if="showMn">
-        <p>
-            {{ mnomonic }}
-        </p>
-        <van-button size="mini" @click="confirmSaveNmonic">
-            我已经保存了
-        </van-button>
-        <van-dialog v-model:show="showMnDialog" 
-                title="请输入助记词" 
-                show-cancel-button
-                @confirm="confirmMn">
-        <van-cell-group inset>
-            <van-field v-model="mnomonicNew"
-                label="助记词"
-                placeholder="请输入助记词" 
-                type="text"
-                />
-        </van-cell-group>
-    </van-dialog>
+    <template v-if="showMn">
+        <p>{{ mnemonic }}</p>
+        <van-button size="mini" @click="confirmSaveMnemonic">我已经保存</van-button>
+        <van-dialog v-model:show="showMnDialog" title="请输入助记词" show-cancel-button @confirm="confirmMn">
+            <van-cell-group inset>
+                <van-field v-model="mnemonic2" label="助记词：" placeholder="请输入助记词" />
+            </van-cell-group>
+        </van-dialog>
     </template>
-    
 </template>
+
 <style lang="less">
-body {
-    padding: 10px;
-}
-p{
+p {
     user-select: all;
 }
 </style>
